9053e37a504ebdca821519738b21bcfe8687bf21
Ehtisham Zahid <ehtishamzahid039@gmail.com>
2024-11-08 22:41:06 +0500

    Changes to be committed: 	deleted:    API/generate/route.js 	new file:   app/api/add/route.js 	modified:   app/generate/page.js 	modified:   lib/mongodb.js 	modified:   package-lock.json 	modified:   package.json

--- app/generate/page.js
+++ app/generate/page.js
@@ -1,23 +1,55 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const Generate = () => {
+    const [link, setLink] = useState("");
+    const [linktext, setLinktext] = useState("");
+    const [handle, setHandle] = useState("");
+
+
+    const addLink = async (req, res, handle) => {
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        const raw = JSON.stringify({
+            "link": link,
+            "linktext": text,
+            "handle": handle
+        });
+
+        const requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow"
+        };
+
+        const r = await fetch("http://localhost:3000/api/add", requestOptions)
+        const result = await r.json();
+        toast(r.message)
+    }
+
     return (
         <div className='min-h-screen grid grid-cols-2'>
-            <div className="col1 flex justify-center items-center flex-col gap-5 text-gray-700">
+            
+            <div className="col1 ml-[10vw] flex justify-center items-center flex-col gap-5 text-gray-700">
                 <div className='flex flex-col gap-5 my-8'>
                     <h1 className='font-bold text-4xl'>Create Your Linktree</h1>
                     <div className="item">
                         <h2 className='font-semibold text-2xl'>Step 1 : Claim your handle</h2>
                         <div className="mx-4">
-                            <input className="px-4 py-2 my-2 focus:outline-pink-500 rounded-3xl" type="text" placeholder='Choose a handle' name="" id="" />
+                            <input value={handle || ""} onChange={e => setHandle(e.target.value)} className="px-4 py-2 my-2 focus:outline-pink-500 rounded-3xl" type="text" placeholder='Choose a handle' name="" id="" />
                         </div>
                     </div>
 
                     <div className="item">
                         <h2 className='font-semibold text-2xl'>Step 2 : Add links</h2>
                         <div className="mx-4">
-                            <input className="px-4 py-2 mx-2 my-2 focus:outline-pink-500 rounded-3xl " type="text" placeholder='Enter link text' />
-                            <input className="px-4 py-2 mx-2 my-2 focus:outline-pink-500 rounded-3xl " type="text" placeholder='Enter link ' />
+                            <input value={handle || ""} onChange={e => setLink(e.target.value)} className="px-4 py-2 mx-2 my-2 focus:outline-pink-500 rounded-3xl " type="text" placeholder='Enter link text' />
+                            <input  className="px-4 py-2 mx-2 my-2 focus:outline-pink-500 rounded-3xl " type="text" placeholder='Enter link ' />
                             <button className='py-2 mx-2 bg-slate-700 text-white rounded-3xl p-5'>Add Link</button>
                         </div>
                     </div>
@@ -30,8 +62,9 @@ const Generate = () => {
                     </div>
                 </div>
             </div>
-            <div className="col2">
-                <img className='h-full object-contain' src="/generate.png" alt="generate your links" />
+            <div className="col2 mr-[10vw]">
+                <img className='h-full  object-contain' src="/generate.png" alt="generate your links" />
+                <ToastContainer />
             </div>
         </div>
     )
