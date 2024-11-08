98c4e89a8325c0a83fbed9a24577e58711aaee5b
Ehtisham Zahid <ehtishamzahid039@gmail.com>
2024-11-08 23:38:58 +0500

    Changes to be committed: 	modified:   app/generate/page.js

--- app/generate/page.js
+++ app/generate/page.js
@@ -10,7 +10,7 @@ const Generate = () => {
     const [handle, setHandle] = useState("");
 
 
-    const addLink = async (req, res, handle) => {
+    const addLink = async (text, link, handle) => {
         const myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
 
@@ -34,7 +34,7 @@ const Generate = () => {
 
     return (
         <div className='min-h-screen grid grid-cols-2'>
-            
+
             <div className="col1 ml-[10vw] flex justify-center items-center flex-col gap-5 text-gray-700">
                 <div className='flex flex-col gap-5 my-8'>
                     <h1 className='font-bold text-4xl'>Create Your Linktree</h1>
@@ -49,15 +49,16 @@ const Generate = () => {
                         <h2 className='font-semibold text-2xl'>Step 2 : Add links</h2>
                         <div className="mx-4">
                             <input value={handle || ""} onChange={e => setLink(e.target.value)} className="px-4 py-2 mx-2 my-2 focus:outline-pink-500 rounded-3xl " type="text" placeholder='Enter link text' />
-                            <input  className="px-4 py-2 mx-2 my-2 focus:outline-pink-500 rounded-3xl " type="text" placeholder='Enter link ' />
+                            <input className="px-4 py-2 mx-2 my-2 focus:outline-pink-500 rounded-3xl " type="text" placeholder='Enter link ' />
                             <button className='py-2 mx-2 bg-slate-700 text-white rounded-3xl p-5'>Add Link</button>
                         </div>
                     </div>
 
                     <div className="item">
                         <h2 className='font-semibold text-2xl'>Step 3 : Add Picture and finalize</h2>
-                        <div className="mx-4">
+                        <div className="mx-4 flex flex-col">
                             <input className="px-4 py-2 mx-2 my-2 focus:outline-pink-500 rounded-3xl " type="text" placeholder='Enter link to your picture' />
+                            <button className='p-5 py-2 w-fit my-5 bg-slate-900 text-white font-bold rounded-3xl'>Create your linktree</button>
                         </div>
                     </div>
                 </div>
