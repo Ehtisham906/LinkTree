834e7168861d7820fee4d10b41250506af1e75a8
Ehtisham Zahid <ehtishamzahid039@gmail.com>
2024-11-09 15:37:59 +0500

    adjusments

--- app/api/add/route.js
+++ app/api/add/route.js
@@ -4,11 +4,17 @@ import clientPromise from "@/lib/mongodb";
 export async function POST(request) {
 
     const body = await request.json();
-    
+
     const client = await clientPromise;
     const db = client.db("linktree");
     const collection = db.collection("links");
 
+    // 
+    const doc = await collection.findOne({ handle: body.handle })
+
+    if (doc) {
+        return Response.json({ success: false, error: true, message: 'This linktree already exists', result: null });
+    }
     const result = await collection.insertOne(body);
-    return Response.json({ success: true, error: false, message: 'Added!', result: result });
+    return Response.json({ success: true, error: flase, message: 'Your linktree is generated.', result: result });
 }
--- app/generate/page.js
+++ app/generate/page.js
@@ -5,20 +5,39 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 const Generate = () => {
-    const [link, setLink] = useState("");
-    const [linktext, setLinktext] = useState("");
+    // const [link, setLink] = useState("");
+    // const [linktext, setLinktext] = useState("");
+    const [links, setLinks] = useState([{ link: "", linktext: "" }])
     const [handle, setHandle] = useState("");
+    const [pic, setPic] = useState("");
 
+    const handleChange = (index, link, linktext) => {
+        setLinks((initalLinks) => {
+            return initalLinks.map((item, i) => {
+                if (i == index) {
+                    return { link, linktext }
+                }
+                else {
+                    return item
+                }
+            })
+        })
+    }
 
-    const addLink = async (text, link, handle) => {
+    const addLink = () => {
+        setLinks(links.concat([{ link: "", linktext: "" }]))
+    }
+
+    const sumbitLinks = async (text, link, handle) => {
         const myHeaders = new Headers();
         myHeaders.append("Content-Type", "application/json");
 
         const raw = JSON.stringify({
-            "link": link,
-            "linktext": text,
-            "handle": handle
+            "links": links,
+            "handle": handle,
+            "pic": pic
         });
+        console.log(raw);
 
         const requestOptions = {
             method: "POST",
@@ -29,7 +48,17 @@ const Generate = () => {
 
         const r = await fetch("http://localhost:3000/api/add", requestOptions)
         const result = await r.json();
-        toast(r.message)
+        if (result.success) {
+            toast.success(result.message)
+            setLinks("");
+            setPic("");
+            setHandle("");
+        }
+        else {
+            toast.error(result.message)
+        }
+
+
     }
 
     return (
@@ -41,24 +70,26 @@ const Generate = () => {
                     <div className="item">
                         <h2 className='font-semibold text-2xl'>Step 1 : Claim your handle</h2>
                         <div className="mx-4">
-                            <input value={handle || ""} onChange={e => setHandle(e.target.value)} className="px-4 py-2 my-2 focus:outline-pink-500 rounded-3xl" type="text" placeholder='Choose a handle' name="" id="" />
+                            <input value={handle || ""} onChange={e => { setHandle(e.target.value) }} className="px-4 py-2 my-2 focus:outline-pink-500 rounded-3xl" type="text" placeholder='Choose a handle' name="" id="" />
                         </div>
                     </div>
 
                     <div className="item">
                         <h2 className='font-semibold text-2xl'>Step 2 : Add links</h2>
-                        <div className="mx-4">
-                            <input value={handle || ""} onChange={e => setLink(e.target.value)} className="px-4 py-2 mx-2 my-2 focus:outline-pink-500 rounded-3xl " type="text" placeholder='Enter link text' />
-                            <input className="px-4 py-2 mx-2 my-2 focus:outline-pink-500 rounded-3xl " type="text" placeholder='Enter link ' />
-                            <button className='py-2 mx-2 bg-slate-700 text-white rounded-3xl p-5'>Add Link</button>
-                        </div>
+                        {links && links.map((item, index) => {
+                            return <div key={index} className="mx-4">
+                                <input value={item.link || ""} onChange={e => { handleChange(index, e.target.value, item.linktext) }} className="px-4 py-2 mx-2 my-2 focus:outline-pink-500 rounded-3xl " type="text" placeholder='Enter link' />
+                                <input value={item.linktext || ""} onChange={e => { handleChange(index, item.link, e.target.value) }} className="px-4 py-2 mx-2 my-2 focus:outline-pink-500 rounded-3xl " type="text" placeholder='Enter link text' />
+                            </div>
+                        })}
+                        <button onClick={() => addLink()} className='py-2 mx-2 bg-slate-700 text-white rounded-3xl p-5'>+ Add Link</button>
                     </div>
 
                     <div className="item">
                         <h2 className='font-semibold text-2xl'>Step 3 : Add Picture and finalize</h2>
                         <div className="mx-4 flex flex-col">
-                            <input className="px-4 py-2 mx-2 my-2 focus:outline-pink-500 rounded-3xl " type="text" placeholder='Enter link to your picture' />
-                            <button className='p-5 py-2 w-fit my-5 bg-slate-900 text-white font-bold rounded-3xl'>Create your linktree</button>
+                            <input value={pic || ""} onChange={e => { setPic(e.target.value) }} className="px-4 py-2 mx-2 my-2 focus:outline-pink-500 rounded-3xl " type="text" placeholder='Enter link to your picture' />
+                            <button disabled={pic == "" || handle == "" || links[0].linktext == ""} onClick={() => { sumbitLinks() }} className='disabled:bg-slate-500 p-5 py-2 w-fit my-5 bg-slate-900 text-white font-bold rounded-3xl'>Create your linktree</button>
                         </div>
                     </div>
                 </div>
